114400bb57b4d66f32869aa63fafc46f4e20ccb0
Danilo Krstonijevic <dlrstonijevic@yahoo.com>
2016-07-28 14:21:50 +0200

    dorada borsmi projekta

--- js/main.js
+++ js/main.js
@@ -1,1 +1,62 @@
+$(document).ready(function () {
 
+    //Podesavanje funkcije za brzinu automatske promene slike
+    $('.carousel').carousel({
+        interval: 6000 //changes the speed
+    });
+
+    //Funkcija za tabulator
+    $('#myTabs a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+
+
+    $('#myTabs a[href="#profile"]').tab('show'); // Select tab by name
+    $('#myTabs a:first').tab('show'); // Select first tab
+    $('#myTabs a:last').tab('show'); // Select last tab
+    $('#myTabs li:eq(2) a').tab('show'); // Select third tab (0-indexed)
+
+    //Funkcija za Slider sa 4 slike
+    
+
+
+
+});
+$(document).ready(function () {
+    $('.userComment').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
